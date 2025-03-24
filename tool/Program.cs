37954eef7d365203671c5fbe989c516b736c2efb
Arve Svendsen <arve.svendsen@outlook.com>
2025-03-24 10:10:31 +0100

    Add initial project structure and configuration files

---
+++ tool/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.CommandLine;
+using System.CommandLine.Invocation;
+
+var rootCommand = new RootCommand
+{
+    new Option<string>(
+        "--name",
+        description: "Your name")
+};
+
+rootCommand.Description = "Sample app for parsing command line arguments";
+
+rootCommand.Handler = CommandHandler.Create<string>((name) =>
+{
+    Console.WriteLine($"Hello, {name}!");
+});
+
+await rootCommand.InvokeAsync(args);
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
